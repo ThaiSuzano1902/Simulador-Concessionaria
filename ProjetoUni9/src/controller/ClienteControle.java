49da7eb3d65a8d67e8ffb2cfb9386b80b8260f54
ThaiSuzano1902 <thaisasnow2@gmail.com>
2026-05-27 18:43:27 -0300

    Refatorazão das pendencias das classes modelos

--- ProjetoUni9/src/controller/ClienteControle.java
+++ ProjetoUni9/src/controller/ClienteControle.java
@@ -7,4 +7,5 @@ import java.util.List;
 public class ClienteControle {
     private List<Cliente> listaCliente = new ArrayList<>();
     
+    
 }
--- ProjetoUni9/src/model/SistemasDeVendasVeiculos.java
+++ ProjetoUni9/src/model/SistemasDeVendasVeiculos.java
@@ -3,7 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SistemasDeVendasVe {
+public class SistemasDeVendasVeiculos {
 
     // ======================================
     // LISTA DE VEÍCULOS
