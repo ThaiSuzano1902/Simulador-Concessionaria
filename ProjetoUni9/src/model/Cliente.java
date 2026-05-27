7e3607bf796cb193c3a0dbf4e9f304be90c91aef
JhowQT <jhon.q.torrez@gmail.com>
2026-05-26 21:57:16 -0300

    organizaçaõ das pastas

--- ProjetoUni9/src/model/Cliente.java
+++ ProjetoUni9/src/model/Cliente.java
@@ -1,11 +1,12 @@
-package model;
+import Enums.TipoCliente;
 
 public class Cliente {
+    //Guarda dados do cliente
 
-    //Guarda o cadastro do cliente
     private long id;
     private String nome;
     private String contato;
     private String cadastro;
     private TipoCliente tipoCliente;
+
 }
